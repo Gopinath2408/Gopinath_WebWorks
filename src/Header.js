eb77c33dfe3b29364ae0bafe1a386d88f2dd0ae2
Gopi nath <you@example.com>
2024-06-16 07:12:47 +0530

    initial commit

--- src/Header.js
+++ src/Header.js
@@ -2,19 +2,25 @@ import React from 'react';
 import profilePhoto from './images/professional_photo.jpg'; // Replace with your actual profile photo file path or URL
 
 const Header = () => {
+  const linkStyle = {
+    color: '#007bff', 
+    textDecoration: 'none',
+    
+  };
   return (
     <header style={headerStyle}>
       <div style={imageContainer}>
         <img src={profilePhoto} alt="Profile" style={imageStyle} />
       </div>
       <div style={contentContainer}>
-        <h1 style={headingStyle}>Gopinath</h1>
-        <p style={textStyle}>5/18/63, Madhaiyan Nagar, Kulaiyur/Gonur(PO) Mettur(TK) Salem – 636 404.</p>
-        <p style={textStyle}>Email-Id: <a href="mailto:gopinath240803@gmail.com">gopinath240803@gmail.com</a></p>
-        <p style={textStyle}>GitHub: <a href="https://github.com/Gopinath2408">https://github.com/Gopinath2408</a></p>
-        <p style={textStyle}>LinkedIn: <a href="https://www.linkedin.com/in/gopinath-sakthivel-b53964224">https://www.linkedin.com/in/gopinath-sakthivel-b53964224</a></p>
-        <p style={textStyle}>Phone No: 9994863872</p>
-      </div>
+      <h1 style={headingStyle}>Gopinath</h1>
+      <p style={{ ...textStyle, color: 'orange' }}>K.S.Rangasamy College of Technology</p>
+      <p style={textStyle}>Naukri: <a href="https://www.naukri.com/mnjuser/profile?id=&altresid" style={{ ...linkStyle, color: 'blue' }}>https://www.naukri.com/mnjuser/profile?id=&altresid</a></p>
+      <p style={textStyle}>Email-Id: <a href="mailto:gopinath240803@gmail.com" style={{ ...linkStyle, color: 'blue' }}>gopinath240803@gmail.com</a></p>
+      <p style={textStyle}>GitHub: <a href="https://github.com/Gopinath2408" style={{ ...linkStyle, color: 'blue' }}>https://github.com/Gopinath2408</a></p>
+      <p style={textStyle}>LinkedIn: <a href="https://www.linkedin.com/in/gopinath-sakthivel-b53964224" style={{ ...linkStyle, color: 'blue' }}>https://www.linkedin.com/in/gopinath-sakthivel-b53964224</a></p>
+      <p style={textStyle}>Phone No: <span style={{ ...textStyle, color: 'blue', fontWeight: 'bold' }}>9994863872</span></p>
+    </div>
     </header>
   );
 };
